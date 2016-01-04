9a59ef3dd1f5dc92ac488ff9741080f1b2bfd146
wangshikun2010@gmail.com <wangshikun2010>
2016-01-04 10:49:46 +0800

    init commit

---
+++ jquery/jquery.js
@@ -0,0 +1,16 @@
+(function() {
+
+    var jQuery = window.jQuery = window.$ = function() {}
+
+    jQuery.fn = jQuery.prototype = {};
+
+    // jQuery
+    jQuery.extend = jQuery.fn.extend = function() {}
+
+    // jQuery上添加方法
+    jQuery.extend({});
+
+    // jQuery实例上添加方法
+    jQuery.fn.extend({});
+
+}());
